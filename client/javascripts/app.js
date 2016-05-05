6299fd2c2dced9450e0c901f28fe5d2be46ad030
mittman <mittman@users.noreply.github.com>
2016-05-04 23:50:05 -0700

    Completely broken

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,56 +1,23 @@
-var main = function (toDoObjects) {
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+/* global ko: true, io: true, console: true */
+
+var main = function () {
     "use strict";
 
     var socket = io.connect();
+
+    var toDoObjects = [ {} ];
     var toDos = {};
     var read = 0;
     var onload = true;
 
     console.log("SANITY CHECK");
 
-    socket.on("connect", function() {
-        socket.emit("adduser");
-        socket.emit("get");
-    });
-
-    socket.on("usercount", function(data) {
-        console.log("clients", data);
-        $(".users").html("<span>Users connected: " + data + "</span>");
-    });
-
-    socket.on("todo", function(data) {
-        toDoObjects = data;
-
-        toDos = toDoObjects.map(function (toDo) {
-            // debugging
-            //console.log("desc", toDo.description);
-
-            if (onload) {
-                //console.log("onload", read);
-                ++read;
-            }
-
-
-            return toDo.description;
-        });
-
-        console.log("compare", toDos.length + ":" + read);
-        if (toDos.length != read) {
-            var newItems = toDos.length - read;
-            $(".alerts").html("<span>" + newItems + " new alert(s)!<i class='fa fa-bell-o fa-2x'></i></span>");
-        }
-        else {
-            $(".alerts").empty();
-        }
-
-        $(".tabs a span").toArray().forEach(function (element) {
-            populate(element);
-        });
-
-        $(".tabs .active").trigger("click");
-
-        onload = false;
-    });
+    $("#tab1").show();
+    $("#tab2").hide();
+    $("#tab3").hide();
+    $("#tab4").hide();
 
     function populate(element) {
         var $element = $(element);
@@ -58,21 +25,23 @@ var main = function (toDoObjects) {
         // create a click handler for this element
         $element.on("click", function () {
             var $content,
-                $input,
-                $button,
                 i;
 
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
-            $("main .content").empty();
+            //$("main .content").empty();
 
             // tab 1
             if ($element.parent().is(":nth-child(1)")) {
-                $content = $("<ul>");
+                $("#tab1").show();
+                $("#tab2").hide();
+                $("#tab3").hide();
+                $("#tab4").hide();
+                /*$content = $("<ul>");
                 for (i = toDos.length-1; i >= 0; i--) {
                     $content.append($("<li>").text(toDos[i]));
                 }
-                $("main .content").append($content);
+                $("main .content").append($content);*/
             }
 
             // tab 2
@@ -82,7 +51,10 @@ var main = function (toDoObjects) {
                     $content.append($("<li>").text(todo));
                 });
 
-                $("main .content").append($content);
+                $("#tab2").show();
+                $("#tab1").hide();
+                $("#tab3").hide();
+                $("#tab4").hide();
             }
 
             // tab 3
@@ -120,61 +92,105 @@ var main = function (toDoObjects) {
                         $content.append($li);
                     });
 
-                    $("main .content").append($tagName);
-                    $("main .content").append($content);
                 });
+
+                $("#tab3").show();
+                $("#tab1").hide();
+                $("#tab2").hide();
+                $("#tab4").hide();
             }
 
             // tab 4
             else if ($element.parent().is(":nth-child(4)")) {
-                var $description = $("<input>").addClass("description"),
-                    $inputLabel = $("<p>").text("Description: "),
-                    $tagInput = $("<input>").addClass("tags"),
-                    $tagLabel = $("<p>").text("Tags: "),
-                    $submit = $("<span>").text("+");
-
-                $submit.on("click", function () {
-                    var description = $description.val(),
-                        tags = $tagInput.val().split(","),
-                        newToDo = {"description":description, "tags":tags};
-
-                    // save to DB
-                    socket.emit("post", newToDo);
-                    console.log("POST!");
-                    toDoObjects.push(newToDo);
-                    ++read;
-
-                    // update toDos
-                    toDos = toDoObjects.map(function (toDo) {
-                        return toDo.description;
-                    });
-
-                    // clear input
-                    $description.val("");
-                    $tagInput.val("");
-                });
-
-                $content = $("<div>").append($inputLabel)
-                                     .append($description)
-                                     .append($tagLabel)
-                                     .append($tagInput)
-                                     .append($submit);
-
-                $("main .content").append($content);
+                $("#tab4").show();
+                $("#tab1").hide();
+                $("#tab2").hide();
+                $("#tab3").hide();
             }
 
             return false;
         });
     }
 
+    socket.on("connect", function() {
+        socket.emit("adduser");
+        socket.emit("get");
+    });
+
+    socket.on("usercount", function(data) {
+        console.log("clients", data);
+        $(".users").html("<span>Users connected: " + data + "</span>");
+    });
+
+    socket.on("todo", function(data) {
+        toDoObjects = data;
+
+        toDos = toDoObjects.map(function (toDo) {
+            // debugging
+            //console.log("desc", toDo.description);
+
+            if (onload) {
+                //console.log("onload", read);
+                ++read;
+            }
+
+            return toDo.description;
+        });
+
+        console.log("compare", toDos.length + ":" + read);
+        if (toDos.length !== read) {
+            var newItems = toDos.length - read;
+            $(".alerts").html("<span>" + newItems + " new alert(s)!<i class='fa fa-bell-o fa-2x'></i></span>");
+        }
+        else {
+            $(".alerts").empty();
+        }
+
+        $(".tabs a span").toArray().forEach(function (element) {
+            populate(element);
+        });
+
+        $(".tabs .active").trigger("click");
+
+        onload = false;
+    });
+
     $(".tabs a span").toArray().forEach(function (element) {
         populate(element);
     });
 
     $(".tabs a:first-child span").trigger("click");
+
+    function TaskList(data) {
+       var self = this;
+       self.listitem = ko.observable(data.description);
+       self.tagitem = ko.observable(data.tags);
+    }
+
+    function TaskListViewModel() {
+        var self = this;
+        self.toDoObjects = ko.observableArray([]);
+        self.description = ko.observable();
+        self.tags = ko.observable();
+
+        self.addTask = function() {
+
+            // save to DB
+            socket.emit("post", newToDo);
+            console.log("POST!");
+            self.toDoObjects.push(new TaskList(self.description(), self.tags()));
+            self.description("");
+            self.tags("");
+            ++read;
+
+            // update toDos
+            toDos = toDoObjects.map(function (toDo) {
+                return toDo.description;
+            });
+        };
+    }
+
+    ko.applyBindings(new TaskListViewModel());
 };
 
-$(document).ready(function () {
-    var toDoObjects = [ {} ];
-    main(toDoObjects);
-});
+$(document).ready(main);
